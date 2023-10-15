43489959642774ddf5ebc40b3fb5cc6a6b753ef1
ventoura <oueslatikhalil1@gmail.com>
2023-10-15 19:29:28 +0200

    Revert "add new LinkedList medium questions"
    This reverts commit 06abdd2b

---
+++ leetcode-questions-solutions/src/main/java/com/smiloutcha/linkedlist/medium/Question19.java
@@ -0,0 +1,43 @@
+package com.smiloutcha.linkedlist.medium;
+
+import com.smiloutcha.linkedlist.datastructures.ListNode;
+
+public class Question19 {
+
+
+
+    public  ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode reversedhead = reverse(head);
+        ListNode temp  = reversedhead ;
+        int index = 1 ;
+        if (n == 1 && temp.next == null) {
+            return null;
+        } else if (n == 1) {
+            return reverse(reversedhead.next);
+        }
+        while (temp != null && temp.next != null) {
+            if(index + 1 == n) {
+                temp.next = temp.next.next ;
+                break ;
+            }
+            index ++ ;
+            temp = temp.next ;
+        }
+        return reverse(reversedhead);
+
+
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode previousNode = null;
+        ListNode currentNode = head ;
+        ListNode nextNode = head ;
+        while (nextNode != null) {
+            nextNode = nextNode.next ;
+            currentNode.next = previousNode ;
+            previousNode = currentNode ;
+            currentNode = nextNode ;
+        }
+        return previousNode ;
+    }
+}
